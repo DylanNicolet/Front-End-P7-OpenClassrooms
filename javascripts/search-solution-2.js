fb748c2ca4857a092d3948354bc4ce26187996cd
DylanLaptop <dylannicolet@hotmail.com>
2021-12-06 16:41:45 +0100

    Added: solution-2 JS code

---
+++ javascripts/search-solution-2.js
@@ -0,0 +1,71 @@
+//DOM element
+const searchBar = document.getElementById('main-searchbar');
+
+//load recipes on each new letter added
+searchBar.addEventListener("keypress", ($event) => {
+    filteredRecipes = recipes;
+
+    if(searchBar.value.length>1){
+        let filterValue = searchBar.value.toLowerCase() + $event.key.toLowerCase();
+
+        
+
+        function filterRecipe(recipe){
+            let recipeName = recipe.name.toLowerCase();
+            let recipeDescription = recipe.description.toLowerCase();
+            let currentRecipeIngredients = recipe.ingredients;
+
+            let currentIngredientsNames = currentRecipeIngredients.map(ingredientFunction);
+            let recipeIngredients = currentIngredientsNames.join();
+
+            function ingredientFunction(ingredient){
+                return ingredient.ingredient.toLowerCase();
+            }
+
+            if(recipeName.includes(filterValue) || recipeDescription.includes(filterValue) || recipeIngredients.includes(filterValue)){
+                return true; 
+            } else{
+                return false;
+            }
+        }
+        
+        filteredRecipes = filteredRecipes.filter(filterRecipe);
+        
+        cardContainer.innerHTML = "";
+        loadRecipeCards();
+    }
+});
+
+searchBar.addEventListener("keydown", ($event) => {
+    if(searchBar.value.length>1 && $event.key == "Backspace"){
+        let uncutFilterValue = searchBar.value.toLowerCase();
+        let filterValue = uncutFilterValue.slice(0, uncutFilterValue.length - 1);
+        filteredRecipes = recipes;
+        
+        function filterRecipe(recipe){
+            let recipeName = recipe.name.toLowerCase();
+            let recipeDescription = recipe.description.toLowerCase();
+            let currentRecipeIngredients = recipe.ingredients;
+
+            let currentIngredientsNames = currentRecipeIngredients.map(ingredientFunction);
+            let recipeIngredients = currentIngredientsNames.join();
+
+            function ingredientFunction(ingredient){
+                return ingredient.ingredient.toLowerCase();
+            }
+
+            if(recipeName.includes(filterValue) || recipeDescription.includes(filterValue) || recipeIngredients.includes(filterValue)){
+                return true; 
+            } else{
+                return false
+            };
+        }
+        
+        filteredRecipes = filteredRecipes.filter(filterRecipe);
+
+        cardContainer.innerHTML = "";
+        loadRecipeCards();
+    }
+})
+
+    
